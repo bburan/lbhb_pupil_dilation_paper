141066cf2417c63d03e7063b0a7c55a1495164b8
Brad Buran <bburan@alum.mit.edu>
2019-01-24 11:16:59 -0800

    Cleanup of code
    Simplifying code to represent models fit using prestim baseline.
    Removing a few testing models (e.g., split delta, etc.).

--- fit_rl_sr.py
+++ fit_rl_sr.py
@@ -13,54 +13,23 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--exclude-silent', action='store_true')
     parser.add_argument('--significant-only', action='store_true')
-    parser.add_argument('--band', action='store_true')
-    parser.add_argument('--reference', choices=['sr', 'prestim'], default='sr')
 
-    group = parser.add_mutually_exclusive_group()
-    group.add_argument('--th-bound', action='store_true')
-    group.add_argument('--th-bound-delta', action='store_true')
-    group.add_argument('--th-delta', action='store_true')
-    group.add_argument('--th-delta-unconstrained', action='store_true')
-
-    parser.add_argument('--swap-pupil', action='store_true')
     args = parser.parse_args()
 
     fit_names = ['rl_sr']
+    model = 'rl_with_sr.stan'
+    data = 'rlf_band'
+    reference = 'prestim'
 
-    if args.th_bound:
-        fit_names.append('th_bound')
-        model = 'rl_with_sr_th_bound.stan'
-    elif args.th_bound_delta:
-        fit_names.append('th_bound_delta')
-        model = 'rl_with_sr_th_bound_split_delta.stan'
-    elif args.th_delta:
-        fit_names.append('th_delta')
-        model = 'rl_with_sr_split_delta.stan'
-    elif args.th_delta_unconstrained:
-        fit_names.append('th_delta_unconstrained')
-        model = 'rl_with_sr_split_delta_unconstrained.stan'
-    else:
-        model = 'rl_with_sr.stan'
-
-    fit_names.append(args.reference)
-    if args.band:
-        fit_names.append('band')
-        data = 'rlf_band'
-    else:
-        data = 'rlf'
-
-    if args.swap_pupil:
-        fit_names.append('swap_pupil')
     if args.exclude_silent:
         fit_names.append('exclude_silent')
     if args.significant_only:
         fit_names.append('significant_only')
     fit_name = '_'.join(fit_names)
 
-    cells, data = load_stan_data(data, reference=args.reference,
+    cells, data = load_stan_data(data, reference,
                                  exclude_silent=args.exclude_silent,
-                                 significant_only=args.significant_only,
-                                 swap_pupil=args.swap_pupil)
+                                 significant_only=args.significant_only)
     model = CachedStanModel(model)
     n_iter = 2000
     fit = model.sampling(data, iter=n_iter, control={'max_treedepth': 14},
